330c3d099584d818c90aa50d955b91adb2598403
shruti <jadhav.shruti2103@gmail.com>
2023-01-18 20:56:25 +0530

    ArrayList created

--- src/com/velocity/Arrayse.java
+++ src/com/velocity/Arrayse.java
@@ -1,10 +1,17 @@
 package com.velocity;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Arrayse {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		List<String> l=new ArrayList<String>();
+        l.add("java");
+        l.add("spring");
+        l.add("hibernet");
+        System.out.println(l);
 	}
 
 }
